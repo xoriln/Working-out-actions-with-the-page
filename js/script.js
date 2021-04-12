e23544cd82d0b1948fb8cdfda399c642f12e07bc
Vlad <vladislavsolyara@gmail.com>
2021-04-12 11:05:37 +0300

    interaction is ready

--- js/script.js
+++ js/script.js
@@ -12,8 +12,6 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
-'use strict';
-
 const movieDB = {
     movies: [
         "Логан",
@@ -24,3 +22,26 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => {
+    item.remove();
+});
+
+genre.textContent = 'драма';
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
